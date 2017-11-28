6854088cd3c12d48fee469c653bbc3d6cade2b0e
Nirpa Rai <RETDEV4@ROUNDEDGE.local>
2017-11-28 15:27:56 +0545

    tinymce added for comment box

--- post/admin.py
+++ post/admin.py
@@ -3,4 +3,9 @@ from . models import Post
 
 # Register your models here.
 
-admin.site.register(Post)
+class TinyMCEAdmin(admin.ModelAdmin):
+	class Media:
+		js = ('/static/js/tiny_mce/tiny_mce.js', '/static/js/tiny_mce/textarea.js',)
+
+
+admin.site.register(Post,TinyMCEAdmin)	
